72544706abde589ef4e13cd67f7194c64d72db1b
imornar <imornar@gmail.com>
2023-09-19 15:29:39 +0200

    add svelte client component

---
+++ src/components/Card.svelte
@@ -0,0 +1,21 @@
+<script>
+    let count = 0;
+    function handleIncrease() {
+        count++
+    }
+</script>
+
+<h1>Svelte is up n' running</h1>
+<button on:click={handleIncrease}>Current count is {count}</button>
+
+<style>
+button {
+    background: blue;
+    cursor: pointer;
+    color: white;
+    font-weight: 700;
+    border: none;
+    padding: 8px 16px;
+    border-radius: 12px;
+}
+</style>
